fced5189d2e5350b4340648bf1a8202da10cb9b0
HarishGokul-15 <83446735+HarishGokul-15@users.noreply.github.com>
2023-06-27 01:14:08 +0530

    Create Half_Adder_Nand.v

---
+++ Half_adder/Half_Adder_Nand.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 27.06.2023 00:41:46
+// Design Name: 
+// Module Name: Half_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Half_adder_nand(
+    input A,
+    input B,
+    output Sum,
+    output Carry
+    );
+    wire x,y,z;
+    //here we could implement in gate level
+    nand(z,A,B);
+    nand(y,A,x);
+    nand(z,B,x);
+    nand(Sum,y,z);
+    nand(Carry,x,x);
+    
+endmodule
